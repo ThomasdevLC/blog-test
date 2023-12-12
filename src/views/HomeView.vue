dfaba2295ec29aa31cf8c585690e61d7eb4ef85c
ThomasdevLC <thomas.lecam@gmail.com>
2023-12-12 13:46:24 +0100

    bottom article txt

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="font-nunito">
     <HeaderNav />
-    <div class="mx-32 my-12">
+    <div class="px-32 py-12">
       <div class="grid grid-cols-3 gap-8">
-        <div class="grid-flow-col col-span-2 row-span-2">
+        <div class="h-100 grid-flow-col col-span-2 row-span-2">
           <img src="../assets/images/landscape.jpg" alt="paysage" class="w-full object-cover" />
           <div class="pt-5">
             <p class="tag">Actualité</p>
-            <h2 class="font-title text-3xl mt-2">La Roumanie encore et toujours</h2>
+            <h2 class="font-title text-5xl mt-2">La Roumanie encore et toujours</h2>
             <p class="mt-2">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut laudantium aspernatur voluptatibus. Ad natus laborum placeat eum delectus aliquid
               expedita debitis, iure illum ea repellat fugiat id facere nam...
@@ -21,9 +21,42 @@
         <div class="">
           <img src="../assets/images/meeting.jpg" alt="paysage" class="h-full object-cover" />
         </div>
-        <div class=""><img src="../assets/images/cab.jpg" alt="paysage" class="h-full object-cover" /></div>
-        <div class=""><img src="../assets/images/meeting.jpg" alt="paysage" class="h-full object-cover" /></div>
-        <div class=""><img src="../assets/images/meeting.jpg" alt="paysage" class="h-full object-cover" /></div>
+        <div class="">
+          <img src="../assets/images/cab.jpg" alt="paysage" class="h-full object-cover" />
+          <div class="pt-5">
+            <p class="tag">Actualité</p>
+            <h2 class="font-title text-3xl mt-2">La Roumanie encore et toujours</h2>
+            <p class="mt-2">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut laudantium aspernatur voluptatibus. Ad natus laborum placeat eum delectus aliquid
+              expedita debitis, iure illum ea repellat fugiat id facere nam...
+            </p>
+            <p class="mt-2">C. Le Cam | 12 Décembre 2023</p>
+          </div>
+        </div>
+        <div class="">
+          <img src="../assets/images/meeting.jpg" alt="paysage" class="h-full object-cover" />
+          <div class="pt-5">
+            <p class="tag">Actualité</p>
+            <h2 class="font-title text-3xl mt-2">La Roumanie encore et toujours</h2>
+            <p class="mt-2">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut laudantium aspernatur voluptatibus. Ad natus laborum placeat eum delectus aliquid
+              expedita debitis, iure illum ea repellat fugiat id facere nam...
+            </p>
+            <p class="mt-2">C. Le Cam | 12 Décembre 2023</p>
+          </div>
+        </div>
+        <div class="">
+          <img src="../assets/images/meeting.jpg" alt="paysage" class="h-full object-cover" />
+          <div class="pt-5">
+            <p class="tag">Actualité</p>
+            <h2 class="font-title text-3xl mt-2">La Roumanie encore et toujours</h2>
+            <p class="mt-2">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut laudantium aspernatur voluptatibus. Ad natus laborum placeat eum delectus aliquid
+              expedita debitis, iure illum ea repellat fugiat id facere nam...
+            </p>
+            <p class="mt-2">C. Le Cam | 12 Décembre 2023</p>
+          </div>
+        </div>
       </div>
     </div>
   </div>
