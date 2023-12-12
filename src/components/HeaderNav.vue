26e13041f290739ad4816803e6f8cfd5d23f06f5
ThomasdevLC <thomas.lecam@gmail.com>
2023-12-12 11:05:10 +0100

    data json created

--- src/components/HeaderNav.vue
+++ src/components/HeaderNav.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="py-4 px-3 bg-black text-white">
-    <div class="flex justify-between">
-      <p>Amitiés Taléa</p>
-      <div class="flex">
+  <div class="px-10 py-4 bg-black text-white">
+    <div class="flex justify-between items-center">
+      <p class="text-3xl">Amitiés Taléa</p>
+      <div class="flex gap-10 text-lg">
         <p>Accueil</p>
         <p>Evenements</p>
         <p>Infos</p>
