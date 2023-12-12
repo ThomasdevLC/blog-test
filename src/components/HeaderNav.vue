7e119aed4e10a10bfc27c5afc3cd49203b05a69f
ThomasdevLC <thomas.lecam@gmail.com>
2023-12-12 10:43:22 +0100

    first commit

---
+++ src/components/HeaderNav.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="py-4 px-3 bg-black text-white">
+    <div class="flex justify-between">
+      <p>Amitiés Taléa</p>
+      <div class="flex">
+        <p>Accueil</p>
+        <p>Evenements</p>
+        <p>Infos</p>
+        <p>Actualités</p>
+        <p>30 ans</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <HeaderNav />
+    <p class="text-3xl">Hello</p>
+  </div>
+</template>
+
+<script setup>
+import HeaderNav from "../components/HeaderNav.vue";
+</script>
+
+<style lang="scss" scoped></style>
