9af3242454c57af3a2f0dddddfc26f2306df1218
ThomasdevLC <thomas.lecam@gmail.com>
2023-12-14 13:04:36 +0100

    home text style

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,74 +2,79 @@
   <div class="font-nunito">
     <HeaderNav />
     <div class="px-32 py-12">
-      <div class="grid grid-cols-3 grid-rows-2 gap-8">
-        <div class="col-span-2 row-span-2">
-          <img src="../assets/images/landscape.jpg" alt="paysage" class="w-full h-auto object-cover" />
+      <div class="flex gap-8">
+        <div class="flex flex-col justify-between">
+          <div class="">
+            <img src="../assets/images/landscape.jpg" alt="paysage" class="w-full h-auto object-cover" />
+          </div>
           <div class="pt-5">
             <p class="tag">Actualité</p>
-            <h2 class="font-title text-5xl mt-2">La Roumanie encore et toujours</h2>
+            <h2 class="font-title font-bold text-5xl mt-2">La Roumanie encore et toujours</h2>
             <p class="mt-2">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut laudantium aspernatur voluptatibus. Ad natus laborum placeat eum delectus aliquid
               expedita debitis, iure illum ea repellat fugiat id facere nam...
             </p>
-            <p class="mt-2">C. Le Cam | 12 Décembre 2023</p>
+            <p class="mt-2"><span class="font-semibold">C. Le Cam </span> | 12 Décembre 2023</p>
           </div>
         </div>
-        <div class="relative text-white">
-          <img src="../assets/images/meeting.jpg" alt="paysage" class="h-full object-cover" />
-          <div class="absolute top-0 h-full bg-black bg-opacity-50 w-full flex flex-col justify-end">
-            <div class="p-3">
-              <p class="tag-second">Actualité</p>
-              <h2 class="font-title text-2xl mt-2">Compte rendu de l’assemblée générale du 28 novembre</h2>
-              <p class="mt-2">C. Le Cam | 12 Décembre 2023</p>
+        <div class="flex flex-col justify-between gap-4">
+          <div class="relative text-white">
+            <img src="../assets/images/meeting.jpg" alt="paysage" class="h-full object-cover" />
+            <div class="absolute top-0 h-full bg-black bg-opacity-50 w-full flex flex-col justify-end">
+              <div class="p-3">
+                <p class="tag-second">Actualité</p>
+                <h2 class="font-title font-bold text-2xl mt-2">Compte rendu de l’assemblée générale du 28 novembre</h2>
+                <p class="mt-2"><span class="font-semibold">C. Le Cam </span> | 12 Décembre 2023</p>
+              </div>
+            </div>
+          </div>
+          <div class="relative text-white">
+            <img src="../assets/images/meeting.jpg" alt="paysage" class="h-full object-cover" />
+            <div class="absolute top-0 h-full bg-black bg-opacity-50 w-full flex flex-col justify-end">
+              <div class="p-3">
+                <p class="tag-second">Actualité</p>
+                <h2 class="font-title font-bold text-2xl mt-2">Compte rendu de l’assemblée générale du 28 novembre</h2>
+                <p class="mt-2"><span class="font-semibold">C. Le Cam </span> | 12 Décembre 2023</p>
+              </div>
             </div>
           </div>
         </div>
-        <div class="relative text-white">
-          <img src="../assets/images/meeting.jpg" alt="paysage" class="h-full object-cover" />
-          <div class="absolute top-0 h-full bg-black bg-opacity-50 w-full flex flex-col justify-end">
-            <div class="p-3">
-              <p class="tag-second">Actualité</p>
-              <h2 class="font-title text-2xl mt-2">Compte rendu de l’assemblée générale du 28 novembre</h2>
-              <p class="mt-2">C. Le Cam | 12 Décembre 2023</p>
-            </div>
-          </div>
-        </div>
-
+      </div>
+      <div class="flex gap-4 mt-12">
         <div class="">
-          <img src="../assets/images/plane.jpg" alt="paysage" class="h-96 w-full object-cover" />
+          <img src="../assets/images/plane.jpg" alt="paysage" class="h-80 w-full object-cover" />
           <div class="pt-5">
             <p class="tag">Actualité</p>
-            <h2 class="font-title text-3xl mt-2">La Roumanie encore et toujours</h2>
+            <h2 class="font-title font-bold text-3xl mt-2">La Roumanie encore et toujours</h2>
             <p class="mt-2">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut laudantium aspernatur voluptatibus. Ad natus laborum placeat eum delectus aliquid
               expedita debitis, iure illum ea repellat fugiat id facere nam...
             </p>
-            <p class="mt-2">C. Le Cam | 12 Décembre 2023</p>
+            <p class="mt-2"><span class="font-semibold">C. Le Cam </span> | 12 Décembre 2023</p>
           </div>
         </div>
         <div class="">
-          <img src="../assets/images/cab.jpg" alt="paysage" class="h-96 w-full object-cover" />
+          <img src="../assets/images/cab.jpg" alt="paysage" class="h-80 w-full object-cover" />
           <div class="pt-5">
             <p class="tag">Actualité</p>
-            <h2 class="font-title text-3xl mt-2">La Roumanie encore et toujours</h2>
+            <h2 class="font-title font-bold text-3xl mt-2">La Roumanie encore et toujours</h2>
             <p class="mt-2">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut laudantium aspernatur voluptatibus. Ad natus laborum placeat eum delectus aliquid
               expedita debitis, iure illum ea repellat fugiat id facere nam...
             </p>
-            <p class="mt-2">C. Le Cam | 12 Décembre 2023</p>
+            <p class="mt-2"><span class="font-semibold">C. Le Cam </span> | 12 Décembre 2023</p>
           </div>
         </div>
         <div class="">
-          <img src="../assets/images/traffic.jpg" alt="paysage" class="h-96 w-full object-cover" />
+          <img src="../assets/images/traffic.jpg" alt="paysage" class="h-80 w-full object-cover" />
           <div class="pt-5">
             <p class="tag">Actualité</p>
-            <h2 class="font-title text-3xl mt-2">La Roumanie encore et toujours</h2>
+            <h2 class="font-title font-bold text-3xl mt-2">La Roumanie encore et toujours</h2>
             <p class="mt-2">
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut laudantium aspernatur voluptatibus. Ad natus laborum placeat eum delectus aliquid
               expedita debitis, iure illum ea repellat fugiat id facere nam...
             </p>
-            <p class="mt-2">C. Le Cam | 12 Décembre 2023</p>
+            <p class="mt-2"><span class="font-semibold">C. Le Cam </span> | 12 Décembre 2023</p>
           </div>
         </div>
       </div>
