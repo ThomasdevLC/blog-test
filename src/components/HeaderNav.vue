f8d03c5f38bafc083d1ef045636624453c4e48e2
ThomasdevLC <thomas.lecam@gmail.com>
2023-12-14 13:19:30 +0100

    header navlink style

--- src/components/HeaderNav.vue
+++ src/components/HeaderNav.vue
@@ -3,11 +3,11 @@
     <div class="flex justify-between items-center">
       <p class="text-3xl">Amitiés Taléa</p>
       <div class="flex gap-10 text-lg">
-        <p>Accueil</p>
-        <p>Événements</p>
-        <p>Infos</p>
-        <p>Actualités</p>
-        <p>30 ans</p>
+        <p :class="{ 'border-b-2 border-white': $route.path === '/' }" class="link">Accueil</p>
+        <p :class="{ 'border-b-2 border-white': $route.path === '/events' }" class="link">Événements</p>
+        <p :class="{ 'border-b-2 border-white': $route.path === '/info' }" class="link">Infos</p>
+        <p :class="{ 'border-b-2 border-white': $route.path === '/news' }" class="link">Actualités</p>
+        <p :class="{ 'border-b-2 border-white': $route.path === '/30years' }" class="link">30 ans</p>
       </div>
     </div>
   </div>
