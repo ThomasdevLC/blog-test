9132bff737ff2e86b3c58dfb502b8de42bf31812
ThomasdevLC <thomas.lecam@gmail.com>
2023-12-14 13:51:30 +0100

    navlinks

--- src/components/HeaderNav.vue
+++ src/components/HeaderNav.vue
@@ -3,16 +3,12 @@
     <div class="flex justify-between items-center">
       <p class="text-3xl">Amitiés Taléa</p>
       <div class="flex gap-10 text-lg">
-        <p :class="{ 'border-b-2 border-white': $route.path === '/' }" class="link">Accueil</p>
-        <p :class="{ 'border-b-2 border-white': $route.path === '/events' }" class="link">Événements</p>
-        <p :class="{ 'border-b-2 border-white': $route.path === '/info' }" class="link">Infos</p>
-        <p :class="{ 'border-b-2 border-white': $route.path === '/news' }" class="link">Actualités</p>
-        <p :class="{ 'border-b-2 border-white': $route.path === '/30years' }" class="link">30 ans</p>
+        <router-link :to="{ path: '/' }" :class="{ 'border-b-2 border-white': $route.path === '/' }" class="link">Accueil</router-link>
+        <router-link :to="{ path: '/events' }" :class="{ 'border-b-2 border-white': $route.path === '/events' }" class="link">Événements</router-link>
+        <router-link :to="{ path: '/info' }" :class="{ 'border-b-2 border-white': $route.path === '/info' }" class="link">Infos</router-link>
+        <router-link :to="{ path: '/news' }" :class="{ 'border-b-2 border-white': $route.path === '/news' }" class="link">Actualités</router-link>
+        <router-link :to="{ path: '/30years' }" :class="{ 'border-b-2 border-white': $route.path === '/30years' }" class="link">30 ans</router-link>
       </div>
     </div>
   </div>
 </template>
-
-<script setup></script>
-
-<style lang="scss" scoped></style>
---
+++ src/views/EventsView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <HeaderNav />
+    <div class="px-32 py-12">
+      <div class="w-full flex">
+        <div class="w-96">
+          <p class="tag">Actualité</p>
+          <h2 class="font-title font-bold text-3xl mt-2">La Roumanie encore et toujours</h2>
+          <p class="mt-2">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut laudantium aspernatur voluptatibus. Ad natus laborum placeat eum delectus aliquid
+            expedita debitis, iure illum ea repellat fugiat id facere nam...
+          </p>
+          <p class="mt-2"><span class="font-semibold">C. Le Cam </span> | 12 Décembre 2023</p>
+        </div>
+        <div class="w-96">
+          <img src="../assets/images/plane.jpg" alt="paysage" class="h-80 w-full object-cover" />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import HeaderNav from "../components/HeaderNav.vue";
+</script>
